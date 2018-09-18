a0198a057ce826a2866d0b26dda67afd1cfc7605
Matthew <matthewhoyan@gmail.com>
2018-09-18 10:26:21 +0800

    oop

---
+++ php/oop/test.php
@@ -0,0 +1,22 @@
+<?php
+
+class Task {
+    public $title;
+    public $description;
+    public $completed = false;
+
+    public function __construct($title, $description = "") {
+        $this->title = $title;
+        $this->description = $description;
+    }
+
+    public function complete() {
+        $this->completed = true;
+    }
+}
+
+$task = new Task('Learn OOP');
+
+$task->complete();
+
+var_dump($task);
